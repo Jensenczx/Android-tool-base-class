04de4fd0ca22692503a16b8be8ecb20b4b54f431
Jensenczx <jensenczx@gmail.com>
2015-08-24 19:04:08 +0800

    基类在日志中输出每个Activity的名字，方便对于页面修改

--- app/src/main/java/com/example/chenjensen/android_tool_base_class/baseclass/BaseActivity.java
+++ app/src/main/java/com/example/chenjensen/android_tool_base_class/baseclass/BaseActivity.java
@@ -2,8 +2,8 @@ package com.example.chenjensen.android_tool_base_class.baseclass;
 
 import android.app.Activity;
 import android.os.Bundle;
-
 import com.example.chenjensen.android_tool_base_class.manager.ActivityManager;
+import com.example.chenjensen.android_tool_base_class.util.LogUtil;
 
 /**
  * Created by chenjensen on 15/8/23.
@@ -13,6 +13,7 @@ public class BaseActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LogUtil.v("ActivityName",this.getClass().getSimpleName());
         ActivityManager.addActivity(this);
     }
 
--- app/src/main/java/com/example/chenjensen/android_tool_base_class/util/LogUtil.java
+++ app/src/main/java/com/example/chenjensen/android_tool_base_class/util/LogUtil.java
@@ -15,27 +15,27 @@ public class LogUtil {
     public static final int NOTHING = 6;
     public static final int LEVEL = VERBOSE;
 
-    public void v(String tag,String msg){
+    public static void v(String tag,String msg){
         if (LEVEL<=VERBOSE)
             Log.v(tag,msg);
     }
 
-    public void d(String tag,String msg){
+    public static void d(String tag,String msg){
         if(LEVEL<=DEBUG)
          Log.d(tag, msg);
     }
 
-    public void i(String tag,String msg){
+    public static void i(String tag,String msg){
         if(LEVEL<=INFO)
             Log.i(tag, msg);
     }
 
-    public void w(String tag,String msg){
+    public static void w(String tag,String msg){
         if(LEVEL<=WARN)
             Log.w(tag, msg);
     }
 
-    public void e(String tag,String msg){
+    public static void e(String tag,String msg){
         if(LEVEL<=ERROR)
             Log.e(tag, msg);
     }
